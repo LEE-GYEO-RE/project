6779625076413313a05efe438a9a26feaa5d8bc2
LEE-GYEO-RE <rufp9891@gmail.com>
2025-07-02 16:48:45 +0900

    0702 1차 커밋

--- js/detail_update.js
+++ js/detail_update.js
@@ -28,8 +28,35 @@ function getpost(){
     for( let i = 0 ; i < posts.length ; i++ ){
         const obj = posts[i];
         if( obj.pid == selectPid ){
-            obj.title = document.querySelector('#titleInput').value;
-            obj.
+            document.querySelector('#titleInput').value = obj.titleInput
+            document.querySelector('#movieTitleInput').value = obj.movieTitleInput
+            document.querySelector('#descInput').value = obj.descInput
+            document.querySelector('#inSpoilerInput').value = obj.isSpoilerInput
+            document.querySelector('#fileInput').value = obj.fileInput
+            document.querySelector('input[name = "rating"]:checked').value = obj.ratingInput
+    
+        } // if end
+    } // for end
+} // func end
+
+// (2) 수정 함수
+function detailUpdate(){
+    const url = new URL.createObjectURL(location.search);     // url 경로 가져오기
+    const selectPid = url.get('pid') 
+    getPosts();
+    
+    for( let i = 0 ; i < posts.length ; i++){
+        const obj = posts[i];
+        if( obj.pid == selectPid ){
+            obj.titleInput = document.querySelector('#titleInput').value;
+            obj.movieTitleInput = document.querySelector('#movieTitleInput').value;
+            obj.descInput = document.querySelector('#descInput').value;
+            obj.isSpoilerInput = document.querySelector('#inSpoilerInput').value;
+            obj.fileInput = document.querySelector('#fileInput').value;
+            obj.ratingInput = document.querySelector('input[name = "rating"]:checked').value;
+            setPosts();
+            alert('수정완료');
+            location.href = `writedetail.html?no=${selectPid}`
         }
     }
 }

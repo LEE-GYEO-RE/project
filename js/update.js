dfccfdd18945e83a9df05a9aeb95e8ad6b2e5399
LEE-GYEO-RE <rufp9891@gmail.com>
2025-07-03 16:42:05 +0900

    css 수정

--- js/update.js
+++ js/update.js
@@ -67,3 +67,36 @@ function back(){
         location.href = 'content.html';
     }
 }
+// 공통 JS (헤더 로그인 구현)
+isLogin();
+function isLogin() {
+    const header_content = document.querySelector("#header_content");
+    let userList = getuserList();
+    let currentUser = new URLSearchParams(location.search).get('uno');
+    let html = '';
+
+    if (currentUser != null) {
+        for (let i=0; i<userList[i].length; i++) {
+            if (userList[i].uno == currentUser) {
+                html += `<ul id="header_top">
+                            <li><a href="list.html?pages=1">로그아웃</a></li>
+                        </ul>
+                        <ul id="header_bottom">
+                            <li>${userList[i].uid}님, 환영합니다</li>
+                        </ul>`;
+            }
+        }
+    } else return;
+
+    header_content.innerHTML = html;
+} // 로그인 여부 확인, 로그인 시 html 변경 (로그아웃, uid 포함으로)
+
+function getUserList() {
+    let userList = localStorage.getItem('userList');
+    if (userList == null) {
+        userList = [];
+    } else {
+        userList = JSON.parse(userList);
+    }
+    return userList;
+} // userList getter

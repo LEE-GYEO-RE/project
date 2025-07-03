1fb83cacd831bac866f813e37a6ec764c86d6116
IceP0tato <rlawodud0313@gmail.com>
2025-07-03 16:41:12 +0900

    작업 진행 사항 공유

--- js/list.js
+++ js/list.js
@@ -3,7 +3,7 @@ function viewList() {
     console.log('viewList start');
     const table_content = document.querySelector(".table_content");
     const main_pages = document.querySelector('#main_pages');
-    const main_buttons = document.querySelector('#main_buttons');
+    
     let postList = getPostList();
     let html = '';
 
@@ -18,7 +18,7 @@ function viewList() {
         for (let i=start; i<end && i<postList.length; i++) {
             html += `<tr>
                         <td class="list_num">${postList[i].pid}</td>
-                        <td class="list_title"><a href="content.html?pid=${postList[i].pid}&uid=${getUid(uid)}">${postList[i].title} ${spoilerCheck(postList[i].isSpoiler)}</a></td>
+                        <td class="list_title"><a href="content.html?pid=${postList[i].pid}&${getUid(uid)}">${postList[i].title} ${spoilerCheck(postList[i].isSpoiler)}</a></td>
                         <td class="list_movieTitle">${postList[i].movieTitle}</td>
                         <td class="list_rating">${makeRating(postList[i].rating)}</td>
                         <td class="list_date">${postList[i].date}</td>
@@ -28,15 +28,11 @@ function viewList() {
 
     let totalPage = postList[postList.length-1].pid;
     for (let i=1; i<=(totalPage/10)+1; i++) {
-        pages += `<a class="page_numbers" href='list.html?pages=${i}&uid=${getUid(uid)}'>${i}</a>`;
+        pages += `<a class="page_numbers" href='list.html?pages=${i}&${getUid(uid)}'>${i}</a>`;
     }
-    
-    let buttons = `<button onclick="deleteList()">[ 삭제 ]</button>
-                    <button><a href="write.html?uid=${getUid(uid)}">[ 글쓰기 ]</a></button>`;
 
     table_content.innerHTML = html;
     main_pages.innerHTML = pages;
-    main_buttons.innerHTML = buttons;
     console.log('viewList end');
 } // 게시글 목록 출력
 
@@ -105,7 +101,6 @@ function isLogin() {
     let userList = getUserList();
     let currentUser = new URLSearchParams(location.search).get('uid');
     let html = '';
-    console.log(currentUser);
 
     if (currentUser != null) {
         for (let i=0; i<userList.length; i++) {
@@ -132,6 +127,21 @@ function getUserList() {
     }
     return userList;
 } // userList getter
+
+drawLogin();
+function drawLogin() {
+    let uid = new URLSearchParams(location.search).get('uid');
+
+    const header_logo = document.querySelector('#header_logo');
+    const main_buttons = document.querySelector('#main_buttons');
+
+    let logo = `<a href="list.html?pages=1&${getUid(uid)}">무비존</a>`;
+    let buttons = `<button onclick="deleteList()">[ 삭제 ]</button>
+                    <button><a href="write.html?${getUid(uid)}">[ 글쓰기 ]</a></button>`;
+
+    header_logo.innerHTML = logo;
+    main_buttons.innerHTML = buttons;
+} // 로그인 체크 후 쿼리스트링 구현
 
 function getUid(uid) {
     // 함수에 let uid = new URLSearchParams(location.search).get('uid'); 로 uid 값을 가져온 다음, 매개변수에 uid를 넣어 호출할 것

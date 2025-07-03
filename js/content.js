afe7c4bf5ed7fe26c33dfc055be785f4f0a81cd0
LEE-GYEO-RE <rufp9891@gmail.com>
2025-07-03 16:03:43 +0900

    수정

--- js/content.js
+++ js/content.js
@@ -8,29 +8,29 @@ function getPosts() {
         postList = []
     } else {
         postList = JSON.parse(postList)
-    }
+    }return postList;
 }
 
 // localStorage 배열 저장하는 함수
-function setPosts() {
+function setPosts(postList) {
     localStorage.setItem('postList', JSON.stringify(postList));
 }
 
 // 조회 함수
 detailBoard();
 function detailBoard() {
-    const url = new URL.createObjectURL(location.search);     // url 경로 가져오기
+    const url = new URLSearchParams(location.search);     // url 경로 가져오기
     const selectPid = url.get('pid');
 
-    getPosts();
+    let postList = getPosts();
 
     for (i = 0; i < postList.length; i++) {
-        const obj = postList.length[i];
-        if (obj.pid == selectPid) {
+        const obj = postList[i];
+        if (postList[i].pid == selectPid) {
             document.querySelector('.title').innerHTML = obj.title                  // 선택된 게시물 번호와 일치하면 내용 출력
             document.querySelector('.review-text').innerHTML = obj.desc      // content.html 의 클래스명 가진 마크업 사이에 찾은 obj.~~` 넣어서 출력
             document.querySelector('.info-user').innerHTML = obj.     // obj에서 user 넣어야 빼오기 가능
-                document.querySelector('.info-rate').innerHTML = obj.rating
+            document.querySelector('.info-rate').innerHTML = obj.rating
             // title , movieTitle , desc , file , isSpoiler , rating , date
         }
     }
@@ -40,10 +40,10 @@ function detailBoard() {
 // (1) 삭제 함수
 function contentDelete() {
 
-    // 이미 선택된 게시글 들어왔는데, 필요할까?    const url = new URL.createObjectURL(location.search);     // url 경로 가져오기
-    //                                          const selectPid = url.get('pid')                            // 선택한 pid 가져오기
+    const url = new URLSearchParams(location.search);     // url 경로 가져오기
+    const selectPid = url.get('pid')                            // 선택한 pid 가져오기
 
-    getPosts();                                                 // localStorage 배열 가져오기
+    let postList = getPosts();                                                 // localStorage 배열 가져오기
 
     // for (let i = 0; i < postList.length; i++) {                  // 배열 내 선택된 게시물 조회
     //     const obj = postList.length[i];
@@ -67,7 +67,7 @@ function postsUpdateView() {
     // 이것도 이미 선택된 게시물이라 필요할까?    const url = new URL.createObjectURL(location.search);     // url 경로 가져오기
     //                                         const selectPid = url.get('pid')                            // 선택한 pid 가져오기
 
-    getPosts();
+    let postList = getPosts();
 
     // for (let i = 0; i < postList.length; i++) {
     //     const obj = postList[i];

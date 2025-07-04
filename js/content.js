5dd259b13d8592e03ad0cc92a4604f22df93708b
LEE-GYEO-RE <rufp9891@gmail.com>
2025-07-04 11:03:48 +0900

    0704  취합 오전

--- js/content.js
+++ js/content.js
@@ -8,30 +8,60 @@ function getPosts() {
         postList = []
     } else {
         postList = JSON.parse(postList)
-    }return postList;
+    } return postList;
 }
 
 // localStorage 배열 저장하는 함수
 function setPosts(postList) {
     localStorage.setItem('postList', JSON.stringify(postList));
 }
+function getUser() {
+    let userList = localStorage.getItem('userList')
+    if (userList == null) {
+        userList = []
+    } else {
+        userList = JSON.parse(userList)
+    } return userList;
+}
+function setUser(userList) {
+    localStorage.setItem('userList', JSON.stringify(userList));
+}
 
+function getUid(uid) {
+    // 함수에 let uid = new URLSearchParams(location.search).get('uid'); 로 uid 값을 가져온 다음, 매개변수에 uid를 넣어 호출할 것
+
+    if (uid == null)
+        return null;
+    else return `uid=${uid}`;
+}
 // 조회 함수
 detailBoard();
 function detailBoard() {
     const url = new URLSearchParams(location.search);     // url 경로 가져오기
     const selectPid = url.get('pid');
+    const selectUid = url.get('uid');
 
     let postList = getPosts();
+    let userList = getUser();
 
-    for (i = 0; i < postList.length; i++) {
+
+    for (let j = 0; j < userList.length; j++) {
+        const obj1 = userList[j];
+        if (userList[j].uid == selectUid) {
+            document.querySelector('.info-user').innerHTML = obj1.uid
+            break;
+        }
+    }
+
+    for (let i = 0; i < postList.length; i++) {
         const obj = postList[i];
         if (postList[i].pid == selectPid) {
             document.querySelector('.title').innerHTML = obj.title                  // 선택된 게시물 번호와 일치하면 내용 출력
             document.querySelector('.review-text').innerHTML = obj.desc      // content.html 의 클래스명 가진 마크업 사이에 찾은 obj.~~` 넣어서 출력
-            document.querySelector('.info-user').innerHTML = obj.uid     // obj에서 user 넣어야 빼오기 가능
+            // document.querySelector('.info-user').innerHTML = obj.uid     // obj에서 user 넣어야 빼오기 가능
             document.querySelector('.rating').innerHTML = obj.rating
             // title , movieTitle , desc , file , isSpoiler , rating , date
+            break;
         }
     }
 }
@@ -41,56 +71,74 @@ function detailBoard() {
 function contentDelete() {
 
     const url = new URLSearchParams(location.search);     // url 경로 가져오기
-    const selectPid = url.get('pid')                            // 선택한 pid 가져오기
+    // const selectPid = url.get('pid')                            // 선택한 pid 가져오기
+    const selectUid = url.get('uid');
 
     let postList = getPosts();                                                 // localStorage 배열 가져오기
+    let userList = getUser();
 
-    // for (let i = 0; i < postList.length; i++) {                  // 배열 내 선택된 게시물 조회
-    //     const obj = postList.length[i];
-    //     if (obj.pid == selectPid) {
-    const confirm = prompt('비밀번호 입력 : ');          // 존재하면 비번 받고, 일치하면 삭제
-    if (confirm == obj.pwd) {
-        postList.splice(i, 1)
-        setPosts(postList);
-        alert('게시물이 삭제되었습니다.')
-        location.href = 'list.html';                   // 삭제 성공시 list.html 로 이동
-    } else {
-        alert('삭제 실패 : 비밀번호 불일치')
-    } // else end
-} // if end
+    for (let j = 0; j < userList.length; j++) {
+        const obj = userList[j];
+        if (obj.uid == selectUid) {
+            const confirm = prompt('비밀번호 입력 : ');          // 존재하면 비번 받고, 일치하면 삭제
+            if (confirm == obj.pwd) {
+                postList.splice(i, 1)
+                setPosts(postList);
+                alert('게시물이 삭제되었습니다.')
+                location.href = `list.html?pages=1&${getUid(obj.uid)}`;                   // 삭제 성공시 list.html 로 이동
+            } else {
+                alert('삭제 실패 : 비밀번호 불일치')
+            } // else end
+        } // if end
+    }
+}    
+
+//     for (let i = 0; i < postList.length; i++) {                  // 배열 내 선택된 게시물 조회
+//         const obj = postList[i];
+//         if (obj.pid == selectPid) {
+//             const confirm = prompt('비밀번호 입력 : ');          // 존재하면 비번 받고, 일치하면 삭제
+//             if (confirm == obj.pwd) {
+//                 postList.splice(i, 1)
+//                 setPosts(postList);
+//                 alert('게시물이 삭제되었습니다.')
+//                 location.href = 'list.html';                   // 삭제 성공시 list.html 로 이동
+//             } else {
+//                 alert('삭제 실패 : 비밀번호 불일치')
+//             } // else end
+//         } // if end
 //     } // for end
 // } // func end
 
 
 // (2) 수정 이동 함수
 function postsUpdateView() {
-    // 이것도 이미 선택된 게시물이라 필요할까?    const url = new URL.createObjectURL(location.search);     // url 경로 가져오기
-    //                                         const selectPid = url.get('pid')                            // 선택한 pid 가져오기
+    const url = new URLSearchParams(location.search);     // url 경로 가져오기
+    const selectPid = url.get('pid')                            // 선택한 pid 가져오기
 
     let postList = getPosts();
 
-    // for (let i = 0; i < postList.length; i++) {
-    //     const obj = postList[i];
-    //     if (obj.pid == selectPid) {
-    const confirm = prompt('비밀번호 입력 : ')
-    if (confirm == obj.pwd) {
-        location.href = `update.html?no=${selectPid}`
-    } else {
-        alert('수정 불가 : 비밀번호 불일치')
-    }
-} // if end
-//     } // for end
-// } // func end
+    for (let i = 0; i < postList.length; i++) {
+        const obj = postList[i];
+        if (obj.pid == selectPid) {
+            const confirm = prompt('비밀번호 입력 : ')
+            if (confirm == obj.pwd) {
+                location.href = `update.html?no=${selectPid}`
+            } else {
+                alert('수정 불가 : 비밀번호 불일치')
+            }
+        } // if end
+    } // for end
+} // func end
 
 function makeRating(rating) {
     let html = '';
-    for (let i=1; i<6; i++) {
-        if (i<=rating) {
+    for (let i = 1; i < 6; i++) {
+        if (i <= rating) {
             html += '★';
         } else {
             html += '☆';
         }
     }
-    
+
     return html;
 } // 별점에 별 그리기
